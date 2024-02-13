13b2a2c1ac2f1022404bc85a39cfb959f780b023
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-13 06:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetters(str){
+	const obj = {};
+
+    // Loop through each letter in the word
+    for (let i = 0; i < str.length; i++) {
+        const letter = str[i];
+        if (!obj[letter]) { 
+            obj[letter] = [];
+        }
+        obj[letter].push(i);
+    }
+
+    return obj;
+}
+const str = prompt("Enter String");
+const mappedObj = mapLetters(str);
+alert(JSON.stringify(mappedObj));
